a557dfc46a64ff55cea5f5b02505a8f32e71b157
Luciano Ramalho <luciano@ramalho.org>
2019-05-21 15:55:37 -0300

    puzzle 1.5.2

---
+++ LBS/1.5.2-src/updates.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+Puzzle: Suppose that 100 threads run the following program concurrently
+(if you are not familiar with Python, the `for` loop runs the update 100 times.):
+
+```python
+for i in range(100):
+    temp = count
+    count = temp + 1
+```
+
+What is the largest possible value of count after all threads have completed?
+
+What is the smallest possible value?
+
+Hint: the first question is easy; the second is not.
+
+"""
+
+import random
+import time
+
+from threading_cleanup import Thread
+
+def update():
+    global count
+    # time.sleep(random.random() / 10)
+    for _ in range(100):
+        temp = count
+        time.sleep(0)
+        count = temp + 1
+
+count = 0
+
+def main():
+    for _ in range(100):
+        Thread(update)
+    print('count:', count)
+
+main()
