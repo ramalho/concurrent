94a465bf003f2c2cbc2cb7ca04306f3f94c1c4e4
Luciano Ramalho <luciano@ramalho.org>
2019-07-04 18:50:13 -0300

    concurrent prime examples

---
+++ primes/processpool.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from concurrent import futures
+import time
+import os
+
+from primes import is_prime, NUMBERS
+
+
+def prime_test(n):
+    t0 = time.perf_counter()
+    return (is_prime(n), time.perf_counter() - t0)
+
+
+def main(workers):
+    print(f'Workers: {workers} processes')
+
+    t0 = time.perf_counter()
+    with futures.ProcessPoolExecutor(max_workers=workers) as executor:
+        future_map = {executor.submit(prime_test, n): n
+                      for n in NUMBERS}
+        for future in futures.as_completed(future_map):
+            n = future_map[future]
+            res, dt = future.result()
+            msg = 'is' if res else 'is not'
+            print(f'({dt:6.3f}s) {n:16d} {msg} prime')
+    dt = time.perf_counter() - t0
+    print(f'Workers: {workers} processes\nTotal time: {dt:0.3f}s')
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) == 2:
+        num_workers = int(sys.argv[1])
+    else:
+        num_workers = os.cpu_count()
+
+    main(num_workers)
