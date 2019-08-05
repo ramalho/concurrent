6b9aa8cdb32c7c8b6e1156d293746115843851e4
Luciano Ramalho <luciano@ramalho.org>
2019-08-04 21:34:28 -0300

    title parser OK

--- 7models/ThreadsLocks/WordCount/python/spike/titles.py
+++ 7models/ThreadsLocks/WordCount/python/spike/titles.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 import sys
+
 import xml.etree.ElementTree as ET
 
-FILE_NAME = 'ptwiki-20190801-pages-articles-multistream.xml'
 
-NS_TAG = '{http://www.mediawiki.org/xml/export-0.10/}'
+LEN_TAG = len('{http://www.mediawiki.org/xml/export-0.10/}')
 
 
 def titles(file_name):
     count = 0
     for _, elem in ET.iterparse(file_name):
-        tag = elem.tag.replace(NS_TAG, '')
+        tag = elem.tag[LEN_TAG:]
         if tag == 'title':
             count += 1
-            print(count, elem.text)
-
+            print(count, elem.text.strip())
+        elem.clear()
 
 titles(sys.argv[1])
